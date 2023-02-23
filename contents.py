7f9fd4748d0545baad1abe6add4add74d3921331
Dlunhappy226 <76965487+Dlunhappy226@users.noreply.github.com>
2023-02-22 18:27:09 -0800

    adding content decode

---
+++ contents.py
@@ -0,0 +1,22 @@
+def decode(content, seperator):
+    splited = content.split(seperator)
+    decoded = []
+    for x in splited:
+        key, sep, value = x.partition("=")
+        decoded.append((key.strip(), value.strip()))
+    return decoded
+
+def header_decode(content):
+    return decode(content, ";")
+
+def content_decode(content):
+    header, sep, body = content.partition(b"\r\n\r\n")
+    headerContent = header.decode().split("\r\n")
+    headerDist = {}
+    for key in headerContent:
+        headerKey, sep, headerCon = key.partition(":")
+        headerDist[headerKey.strip()] = headerCon.strip()
+    return headerDist, body
+
+def form_decode(content):
+    return decode(content, "&")
