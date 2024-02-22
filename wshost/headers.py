c40b4ff0a06eab10eb79e264842d2751dc80c934
Daniel Lun <dlunhappy226@gmail.com>
2024-02-22 15:05:50 -0800

    Add more header

--- wshost/headers.py
+++ wshost/headers.py
@@ -4,13 +4,24 @@ import time
 
 
 SWITCHING_PROTOCOLS = "101 Switching Protocols"
+
 OK = "200 OK"
-TEMPORARY_REDIRECT = "307 Temporary Redirect"
+NO_CONTENT = "204 No Content"
+
+MOVED_PERMANENTLY = "301 Moved Permanently"
+FOUND = "302 Found"
+NOT_MODIFIED = "304 Not Modified"
+
 BAD_REQUEST = "400 Bad Request"
+UNAUTHORIZED = "401 Unauthorized"
+FORBIDDEN = "403 Forbidden"
 NOT_FOUND = "404 Not Found"
 METHOD_NOT_ALLOWED = "405 Method Not Allowed"
+GONE = "410 Gone"
 REQUEST_HEADER_FIELDS_TOO_LARGE = "431 Request Header Fields Too Large"
+
 INTERNAL_SERVER_ERROR = "500 Internal Server Error"
+SERVICE_UNAVAILABLE = "503 Service Unavailable"
 
 
 def encode(status=OK, content=[]):
