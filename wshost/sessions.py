5f02153d0b8cb67fa85dbf47f3c98a1a68f39950
Daniel Lun <dlunhappy226@gmail.com>
2024-03-20 14:25:59 -0700

    Add more method to session

--- wshost/sessions.py
+++ wshost/sessions.py
@@ -4,16 +4,19 @@ import string
 
 sessions = {}
 
-def new_session(name, id_length=32):
+def new_session(value, id_length=32):
     id = "".join(random.SystemRandom().choice(string.ascii_lowercase + string.digits) for _ in range(id_length))
-    sessions[id] = name
+    sessions[id] = value
     return id
 
-def get_session(id):
-    if id not in sessions:
-        return False
-    
+def get_session(id): 
     return sessions[id]
+
+def check_session(id):
+    return id in sessions
+
+def set_session(id, value):
+    sessions[id] = value
 
 def remove_session(id):
     sessions.pop(id)
