9dcd73039e833a445b56f083be993755e5913705
Daniel Lun <dlunhappy226@gmail.com>
2024-03-21 15:47:34 -0700

    Update installer

--- wshost/__main__.py
+++ wshost/__main__.py
@@ -3,8 +3,6 @@ import sys
 import os
 
 
-command = sys.argv
-
 file_download = {
     "main.py": "https://raw.githubusercontent.com/Dlunhappy226/wshost/main/main.py",
     "config.py": "https://raw.githubusercontent.com/Dlunhappy226/wshost/main/config.py",
@@ -14,16 +12,22 @@ file_download = {
 
 def init():
     print("Initing wshost project.")
+
     if not os.path.exists("html"):
         os.makedirs("html")
+
     for x in file_download:
+        print(f"Downloading {x}")
         data = urllib.request.urlopen(file_download[x])
         file = open(x, "wb+")
         file.write(data.read())
-        file.close
+        file.close()
+
     print("Finish copying all file.")
 
 def main():
+    command = sys.argv
+
     if len(command) < 2:
         print("'wshost init' to init a new wshost project.")
     elif command[1] == "init":
@@ -31,4 +35,4 @@ def main():
     elif command[1] == "help":
         print("'wshost init' to init a new wshost project.")
     else:
-        print(f"Command: '{command}' not found.\n'wshost help' for help.")
+        print(f"Command: '{command[1]}' not found.\n'wshost help' for help.")
