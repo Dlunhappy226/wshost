202537c4b77df225f7a9baae008b279ab6850691
Daniel Lun <dlunhappy226@gmail.com>
2024-03-24 16:41:52 -0700

    Add authorization library

---
+++ wshost/auth.py
@@ -0,0 +1,16 @@
+from wshost import responses
+from wshost import headers
+import base64
+
+
+def unauthorized(realm="Acess to the staging site."):
+    responses.Response("", status=headers.UNAUTHORIZED, header=[("WWW-Authenticate", f"Basic realm={realm}")])
+
+def check_auth(request):
+    if "Authorization" in request["header"]:
+        auth_type, sep, auth_key = request["header"]["Authorization"].partition(" ")
+        if auth_type == "Basic":
+            user, sep, password = base64.b64decode(auth_key).decode().partition(":")
+            return user, password
+                      
+    return "", ""
