c4d8740564571ded66e5a09691ed811fa82542a1
Daniel Lun <dlunhappy226@gmail.com>
2024-04-05 19:49:45 -0700

    Add seperator checking to auth library

--- wshost/auth.py
+++ wshost/auth.py
@@ -1,11 +1,18 @@
+from wshost import exceptions
 import base64
 
 
 def get_auth(request):
     if "Authorization" in request["header"]:
         auth_type, sep, auth_key = request["header"]["Authorization"].partition(" ")
+        if not sep:
+            raise exceptions.BadRequest
+        
         if auth_type == "Basic":
             user, sep, password = base64.b64decode(auth_key).decode().partition(":")
+            if not sep:
+                raise exceptions.BadRequest
+            
             return user, password
                       
     return "", ""
