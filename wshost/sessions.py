cf220f98f304e1d945fca0f56775010361319ad9
Daniel Lun <dlunhappy226@gmail.com>
2024-10-03 23:16:16 -0700

    Add expire to sessions

--- wshost/sessions.py
+++ wshost/sessions.py
@@ -1,22 +1,27 @@
 import random
 import string
+import time
 
 
 sessions = {}
 
-def new_session(value, id_length=32):
+def new_session(value, id_length=32, expire=90*24*60):
     id = "".join(random.SystemRandom().choice(string.ascii_lowercase + string.digits) for _ in range(id_length))
-    sessions[id] = value
+    sessions[id] = {"value":value, "expire": time.time + expire}
     return id
 
 def get_session(id): 
     if id not in sessions:
         return None
+
+    if sessions[id]["expire"] >= time.time():
+        remove_session(id)
+        return None
     
-    return sessions[id]
+    return sessions[id]["value"]
 
 def set_session(id, value):
-    sessions[id] = value
+    sessions[id]["value"] = value
 
 def remove_session(id):
     sessions.pop(id)
