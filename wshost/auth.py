5261233d7d8ee5e349850aef423f2e760a76b473
Daniel Lun <dlunhappy226@gmail.com>
2024-10-08 23:24:56 -0700

    Simpify auth library

--- wshost/auth.py
+++ wshost/auth.py
@@ -1,16 +1,17 @@
 from wshost import exceptions
-import base64
+from base64 import b64decode
 
 
 def get_auth(request):
     if "Authorization" in request["header"]:
-        auth_type, sep, auth_key = request["header"]["Authorization"].partition(" ")
-        if not sep:
+        auth_type, _, auth_key = request["header"]["Authorization"].partition(" ")
+        if not auth_key:
             raise exceptions.BadRequest
         
         if auth_type == "Basic":
-            user, sep, password = base64.b64decode(auth_key).decode().partition(":")
-            if not sep:
+            basic_cert = b64decode(auth_key).decode()
+            user, _, password = basic_cert.partition(":")
+            if not password:
                 raise exceptions.BadRequest
             
             return user, password
